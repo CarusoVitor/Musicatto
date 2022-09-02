88663a201cc927407b31daf1621fa18e3b697930
LeoHernandes <leohernandesvasc@gmail.com>
2022-09-02 07:52:06 -0300

    Add annotations lib and @nullable annotation at toMusicSheet

--- src/MusicBLL/MusicTokens/IMusicToken.java
+++ src/MusicBLL/MusicTokens/IMusicToken.java
@@ -1,7 +1,9 @@
 package MusicBLL.MusicTokens;
 
 import MusicBLL.MusicStatusController;
+import org.jetbrains.annotations.Nullable;
 
 public interface IMusicToken {
+    @Nullable
     public String toMusicSheet(MusicStatusController currentMusicStatus);
 }
